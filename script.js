b07276738beeed688200b1e3175a1cecd05277a5
Volha <134659098+VolhaYaf@users.noreply.github.com>
2023-08-19 12:22:43 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+const button = document.querySelector('#btn');
+button.addEventListener('click', calculateAmount);
+
+
+const buttonTip = document.querySelector('#addTip');
+buttonTip.addEventListener('click', showTip);
+
+function showTip(e){
+    e.preventDefault();
+    tip.style.display = 'block';
+}
+
+
+
+
+function calculateAmount(e) {
+    e.preventDefault();
+
+    const bill = document.querySelector('#bill').value;
+    const people = document.querySelector('#people').value;
+    const tip = document.querySelector('#tip').value;
+
+    if (bill === '' || people === '' || people < 1){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error!',
+            text: 'Please enter your information!'
+          });
+    }
+
+    let amountPerPerson = bill/people;
+    console.log(amountPerPerson);
+    
+    let tipPerPerson = (bill * tip / people);
+    console.log(tipPerPerson);
+
+    let totalSum = amountPerPerson + tipPerPerson;
+    console.log(totalSum);
+
+
+    document.querySelector('#dividedBill').textContent = amountPerPerson.toFixed(2);
+
+    document.querySelector('#dividedTip').textContent = tipPerPerson.toFixed(2);
+
+    document.querySelector('#billAndTip').textContent = totalSum.toFixed(2);
+}
